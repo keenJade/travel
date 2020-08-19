0d2e7bff2efa853539951082213af18b7303f8a2
keenjade <972782465@qq.com>
2020-08-19 16:48:44 +0800

    project install

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <!-- 显示的是当前路由地址所对应的内容 -->
+    <router-view />
   </div>
 </template>
-
 <script>
 export default {
   name: 'App'
@@ -12,12 +11,4 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+// 移动端的300ms点击延迟事件---fastclick包--npm install fastclick --save
+import fastClick from 'fastclick'
+import './assets/styles/reset.css'
+// 移动端1像素的问题
+import './assets/styles/border.css'
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
