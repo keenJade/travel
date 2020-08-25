66418b413513540a1335a2d6b57b5344eb155af3
keenjade <972782465@qq.com>
2020-08-25 09:55:58 +0800

    keep-alive update

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
   <div id="app">
-    <!-- 显示的是当前路由地址所对应的内容 -->
-    <router-view />
+   <!--优化性能--keep-alive
+        路由内容被加载过一次之后，就存在内存中，
+        等下次使用的时候，就不需要再去加载 -->
+    <keep-alive>
+      <!-- 显示的是当前路由地址所对应的内容 -->
+      <router-view />
+    </keep-alive>
   </div>
 </template>
 <script>
--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -14,6 +14,7 @@ import HomeIcons from './components/Icons'
 import HomeRecommed from './components/Recommed'
 import HomeWeekend from './components/Weekend'
 import axios from 'axios'
+import { mapState } from 'vuex'
 export default {
   name: 'Home',
   components: {
@@ -25,18 +26,34 @@ export default {
   },
   data () {
     return {
+      lastCity: ''
     }
   },
+  computed: {
+    ...mapState(['city'])
+  },
   mounted () {
+    this.lastCity = this.city
     this.getHomeInfo()
   },
   methods: {
+    // 需要携带参数city,为了在每次选择城市之后将城市传递给接口
+    // 获取对应城市的内容
     getHomeInfo () {
-      axios.get('/api/index.json')
+      axios.get('/api/index.json?city=' + this.city)
         .then(this.getHomeInnfoSucc)
     },
     getHomeInnfoSucc (res) {
       console.log(res)
+    }
+  },
+  // 使用keep-alive的时候会有一个activated的生命周期
+  activated () {
+    // console.log('actvated')
+    // 判断页面城市是否一致，不一致的时候再次发送请求
+    if (this.lastCity !== this.city) {
+      this.lastCity = this.city
+      this.getHomeInfo()
     }
   }
 }
